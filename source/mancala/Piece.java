15c828eea715128d3161be9b35412ae43e5972a8
H-Loesch <h38383838@tamu.edu>
2019-03-20 02:52:22 -0500

    Constructors
    Added constructors for pieces and pits.

--- source/mancala/Piece.java
+++ source/mancala/Piece.java
@@ -1,12 +1,19 @@
 package mancala;
 
+import java.util.Random;
 
 //the little stone pieces that go in the pits
 public class Piece {
+	static Random key = new Random();
 	private static enum Color {	RED, WHITE, PURPLE, BLUE, YELLOW, GREEN };
 	
 	//every piece has a color
 	Color color; 
 	
 	Pit container;
+	
+	Piece(Pit _container) {
+		container = _container;
+		color = Color.values()[key.nextInt(6)]; //this SHOULD assign a random color to the piece. 
+	}
 }
--- source/mancala/Pit.java
+++ source/mancala/Pit.java
@@ -6,12 +6,30 @@ import java.util.Vector;
 //the six pits on each side of the game board (total 12)
 public class Pit {
 
+	
 	private Vector<Piece> contents;
 	public int place; //numerical place on the board 
 	public int x_location; //x-axis location in the representation of the board 
 	public int y_location; //y-axis location in... well, not "physical space", but you get what I mean.
-	public int player; //which player does this pit belong to? 
+	public int player; //which player does this pit belong to?
 	//maybe some variable that notes if this pit has changed, and thus needs to be re-rendered?
+
+	//construct empty pit 
+	public Pit(int _player, int _x_loc, int _y_loc, int _place) {
+		player = _player;
+		x_location = _x_loc; 
+		y_location = _y_loc; 
+		place = _place;
+		contents = new Vector<Piece>();
+	}
+	
+	public Pit(int _player, int _x_loc, int _y_loc, int _place, Vector<Piece> _pieces) {
+		player = _player;
+		x_location = _x_loc; 
+		y_location = _y_loc; 
+		place = _place;
+		contents = _pieces;
+	}
 	
 	public int size() {
 		return contents.size();
@@ -19,7 +37,7 @@ public class Pit {
 	
 	//render the pit and all the stones in it 
 	public void renderPit() {
-		
+		//do something, idk.
 	};
 	
 	//add a piece...? I dunno.
@@ -31,6 +49,5 @@ public class Pit {
 	public void removePiece() {
 		//generate number 0-size(), remove that piece. you know, that way it doesn't remove the most recently-added one every time!
 	}
-	
 	
 }
