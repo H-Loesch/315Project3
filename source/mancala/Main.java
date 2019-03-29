5e39e7eee1baf8e54ab0021432a132569c685c4d
chorton15 <chorton15@tamu.edu>
2019-03-29 13:33:58 -0500

    GameManagerSimulator Bug Fixes

--- source/mancala/Main.java
+++ source/mancala/Main.java
@@ -1,8 +1,23 @@
 package mancala;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		GameManager gm = new GameManager();
-		gm.run();
+		int size;
+		int stones;
+		boolean random = true;
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Size of board: ");
+		size = Integer.parseInt(scanner.next());   //get size of boad
+		System.out.println("Number of stones: ");
+		stones = Integer.parseInt(scanner.next());   //get num stones
+		
+		GameManager gm = new GameManager(size, stones, random);
+		//gm.run();
+		
+		GameManagerSimulator gmSim = new GameManagerSimulator(gm.board);
+		Tree tree = new Tree(gmSim);
 	}
 }
